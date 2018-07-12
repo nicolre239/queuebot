b37e8d327a70a612ab98b452e0b553c5e89347e2
funtik <pavelsabay@gmail.com>
2018-07-12 12:45:17 +0300

    logger init

---
+++ src/main/java/BotConstants.java
@@ -0,0 +1,8 @@
+///для удобства
+public interface BotConstants {
+    String BOT_NAME = "EpamQueueBot";
+    String BOT_TOKEN = "604894063:AAFmwrvtWWLQmA_6tTscqOsIeQY0Cy6LA7A";
+    int timeOnResource = 10000;
+    int timeRepeatRequest = 15000;
+    int MAX_QUEUE_SIZE = 2;
+}
